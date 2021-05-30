c73a4956e29ca5bd8b5d60d5cbb3d65b82efb86a
Agnieszka <agnieszka.blicharska@gmail.com>
2021-05-30 17:02:41 +0200

    final version

--- README.md
+++ README.md
@@ -13,3 +13,31 @@
 7. ? - after ? you can pass variables
 8. name=pawel - key value variable (name - klucz, pawel - value)
 9. & - delimiter to pass more key value parameters
+
+
+## REST API
+- CRUD - create, read, update, delete (operacje które można zrobić na danych)
+- User 
+1. Create
+    - /users (endpoint do stworzenia users. Always plural)
+    - POST method
+    
+2. Read
+    - /users (endpoint) 
+    - /users/<id> (pobierze jednego użytkownika)  
+    - GET method - pobierze wszystkich użytkowników
+    
+3. Update
+    - /users/<id>
+    - PUT method - update completely user
+    - PATCH method - update partially user
+    
+4. Delete
+    - /users/<id>
+    - DELETE method
+   
+##JSON - javascript object notation
+- data structure - most popular for exchanging data between apps
+- XML - structured HTML
+- HTML - unstructured XML
+    
--- app.py
+++ app.py
@@ -1,7 +1,62 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/elo')
-def hello():
-    return "Hello, World!"
+products = [
+    {
+        "id": 1,
+        "name": "tomatoe",
+        "quantity": 3},
+    {
+        "id": 2,
+        "name": "onion",
+        "quantity": 10
+    }
+]
+
+
+def uuid():
+    max_id = 0
+
+    for product in products:
+        if product["id"] > max_id:
+            max_id = product["id"]
+
+    max_id += 1
+
+    return max_id
+
+
+@app.route('/products', methods=["GET"])
+def get_products():
+    return jsonify(products), 200
+
+
+@app.route('/products/<int:idx>', methods=["GET"])
+def get_product(idx):
+    for product in products:
+        if product["id"] == idx:
+            return jsonify(product), 200
+    return f"No data with id:{idx}", 404
+
+
+@app.route('/products/<int:idx>', methods=["DELETE"])
+def delete_product(idx):
+    for product in products:
+        if product["id"] == idx:
+            products.remove(product)
+            return jsonify({}), 200
+    return f"No data with id:{idx}", 404
+
+
+@app.route('/products', methods=["POST"])
+def create_products():
+    data = request.json
+    data["id"] = uuid()
+    products.append(data)
+
+    return jsonify(data), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
